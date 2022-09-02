349cce44ed5ff012719cb649a21e5739081fb0fc
Pedro García Cereijo <pedrogarciacereijo@gmail.com>
2022-09-02 18:10:27 +0200

    Cambios después de la reunión final

--- flutter_tfg/lib/models/authentication.dart
+++ flutter_tfg/lib/models/authentication.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class AuthenticationHelper {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -30,4 +31,32 @@ class AuthenticationHelper {
 
     print('signout');
   }
+
+  Future<void> recuperarPassword(String email) async {
+    await _auth.sendPasswordResetEmail(email: email);
+  }
+
+  String getExceptionText(Exception e) {
+    if (e is PlatformException) {
+      switch (e.message) {
+        case 'There is no user record corresponding to this identifier. The user may have been deleted.':
+          return 'User with this email address not found.';
+          break;
+        case 'The password is invalid or the user does not have a password.':
+          return 'Invalid password.';
+          break;
+        case 'A network error (such as timeout, interrupted connection or unreachable host) has occurred.':
+          return 'No internet connection.';
+          break;
+        case 'The email address is already in use by another account.':
+          return 'This email address already has an account.';
+          break;
+        default:
+          return 'Unknown error occured.';
+      }
+    } else {
+      return 'Unknown error occured.';
+    }
+  }
+
 }
